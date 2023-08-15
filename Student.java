cd3dc0da4453685d571f98e394d4f271e23b3bf6
Gift <gift.maputle@ctucareer.co.za>
2023-08-15 13:43:44 +0200

    Create basic workflow

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    private String studentName;
+    private int studentAge;
+
+    public Student( String studentName, int studentAge ){
+        this.studentName = studentName;
+        this.studentAge = studentAge;
+    }
+}
